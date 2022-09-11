2cabee9eb623a26fecc44281732e2420faf57f0b
Pawel <pawelk0@poczta.onet.pl>
2022-09-11 04:12:45 +0200

    improvements, first (not fully) working version

--- Logger/Logger.cpp
+++ Logger/Logger.cpp
@@ -4,28 +4,96 @@ namespace Log
 
 std::string Logger::defaultPathToLogs = std::filesystem::current_path().string();
 
-Logger::Logger(const std::string& name_, bool immediateWrite_, unsigned bufferSize) :
+Logger::Logger(const std::string& name_, bool appendToFiles_, unsigned bufferSize) :
 	name(name_),
-	immediateWrite(immediateWrite_),
-	pathToLogs(defaultPathToLogs)
+	pathToLogs(defaultPathToLogs),
+	appendToFiles(appendToFiles_)
 {
+	if (bufferSize < MIN_BUFFER_SIZE)
+	{
+		std::cerr << "Buffer size for logger " << name << " cannot be lower than " << MIN_BUFFER_SIZE << "b\n";
+		bufferSize = MIN_BUFFER_SIZE;
+	}
+	flushWhenSizeWasReached = static_cast<unsigned>(bufferSize - 200);
 	buffer.reserve(bufferSize);
+	auto filenameWithPath = pathToLogs + "/" + name + ".txt";
+	if(appendToFiles)
+		file.open(filenameWithPath, std::ios_base::app);
+	else
+		file.open(filenameWithPath);
+	if (!file.good())
+		std::cout << "! Logger \"" << name << "\" cannot open file " << filenameWithPath << "\n";
+}
+
+Logger::~Logger()
+{
+	writeToOfstream();
+	file.close();
+}
+
+void Logger::writeToOfstream(){
+	file.write(&buffer[0], buffer.size());
 }
 
 std::string Logger::getTime()
 {
 	auto time_tValue = time(nullptr);
-	tm currentTime;
+	static tm currentTime;
 	localtime_s(&currentTime, &time_tValue);
 	return std::format(
-		"[{}/{}/{} {}:{}:{}] ",
-		currentTime.tm_mday,
-		currentTime.tm_mon + 1,
+		"[{}/{:02}/{:02} {:02}:{:02}:{:02}]",
 		currentTime.tm_year + 1900,
+		currentTime.tm_mon + 1,
+		currentTime.tm_mday,
 		currentTime.tm_hour,
 		currentTime.tm_min,
 		currentTime.tm_sec
 	);
+}
+
+void Logger::checkBuffer()
+{
+	if (shouldWriteToFile())
+	{
+		writeToOfstream();
+		clearBuffer();
+	}
+}
+
+void Logger::handleOutputWrite(const std::string& str) const
+{
+	if (isWritingToOutputEnabled())
+		*output << str;
+}
+
+Logger::LineBreaker Logger::operator<<(const std::string& s) {
+	void checkBuffer();
+	if (isWritingToOutputEnabled())
+		lastBufferSize = buffer.size();
+	return LineBreaker(*this) << getTime() << " " << s;
+}
+
+Logger::LineBreaker::LineBreaker(LineBreaker&& rval) noexcept : parent(rval.parent)
+{
+	rval.passed = true;
+}
+
+Logger::LineBreaker::~LineBreaker() {
+	if (!passed)
+	{
+		parent.buffer.push_back('\n');
+		if (parent.lastBufferSize)
+		{
+			size_t messageLen = parent.buffer.size() - parent.lastBufferSize;
+			parent.output->write( &parent.buffer[parent.lastBufferSize], messageLen);
+		}
+	}
+}
+
+Logger::LineBreaker Logger::LineBreaker::operator<<(const std::string& s)
+{
+	parent.buffer.insert(parent.buffer.end(), s.begin(), s.end());
+	return std::move(*this);
 }
 
 
--- Logger/Logger.h
+++ Logger/Logger.h
@@ -3,111 +3,178 @@
 #include <format>
 #include <string>
 #include <vector>
+#include <fstream>
 #include <filesystem>
 #include <type_traits>
 #include <time.h>
 
 namespace Log{
 
-	constexpr auto debug =		"[debug] ";
-	constexpr auto info =		"[info] ";
-	constexpr auto trace =		"[trace] ";
-	constexpr auto warning =	"[warning] ";
-	constexpr auto error =		"[error] ";
-	constexpr auto critical =	"[critical] ";
+	constexpr auto DEBUG =		"[debug] ";
+	constexpr auto INFO =		"[info] ";
+	constexpr auto TRACE =		"[trace] ";
+	constexpr auto WARNING =	"[warning] ";
+	constexpr auto ERROR =		"[error] ";
+	constexpr auto CRITICAL =	"[critical] ";
+
+	static unsigned constexpr MIN_BUFFER_SIZE = 500u;
 
 class Logger
 {
-public:
 	static unsigned constexpr defaultBufferSize = 8192u;
 	static std::string defaultPathToLogs;
 
 	std::string name;
 	std::string pathToLogs;
 	std::vector<char> buffer;
-	bool immediateWrite = false;
-	
-public:
-	explicit Logger(const std::string& name, bool immediateWrite = false, unsigned bufferSize = defaultBufferSize);
-
-	inline void setBufferSize(unsigned newSize)							{ buffer.reserve(newSize); }
-	[[nodiscard]] inline auto getBufferSize() const						{ return buffer.capacity(); }
-	inline void clearBuffer()											{ buffer.clear(); }
-
-	[[nodiscard]] inline const auto& getPathToLogs() const				{ return pathToLogs; }
-	inline void setPathToLogs(const std::string& path)					{ pathToLogs = path; }
-	inline void setPathToLogs(std::string&& path)						{ pathToLogs = std::move(path); }
-
-	[[nodiscard]] inline static const auto& getDefaultPathToLogs()		{ return defaultPathToLogs; }
-	inline static void setDefaultPathToLogs(const std::string& path)	{ defaultPathToLogs = path; }
-	inline static void setDefaultPathToLogs(std::string&& path)			{ defaultPathToLogs = std::move(path); }
-
-	template <typename T>
-	static std::string asString(T&& value)
-	{
-		if constexpr (std::is_same<std::decay<T>::type, const char*>::value || std::is_same<T, std::string>::value)
-			return value;
-		if constexpr (std::is_same<T, char>::value || std::is_same<T, wchar_t>::value)
-			return { value };
-		if constexpr (std::is_arithmetic<T>::value)
-			return std::to_string(value);
-		return "<UNSUPPORTED TYPE>";
-	}
-	
-	struct LineBreaker
-	{
-		std::vector<char>* buffer;
-		bool passed = false;
-		LineBreaker(std::vector<char>* v) : buffer(v) {}
-		LineBreaker(const LineBreaker&) = delete;
-		LineBreaker(LineBreaker&& rval) noexcept
-		{
-			buffer = rval.buffer;
-			rval.passed = true;
-		}
-		~LineBreaker() {
-			if (!passed) buffer->push_back('\n');
-		}
-		LineBreaker operator<<(const std::string& s) {
-			buffer->insert(buffer->end(), s.begin(), s.end());
-			return std::move(*this);
-		}
-	};
-	LineBreaker operator<<(const std::string& s) {
-		return LineBreaker(&buffer) << getTime() << s;
-	}
+	std::ofstream file;
+	std::ostream* output = &std::cout;
+	size_t lastBufferSize = 0;
+	unsigned flushWhenSizeWasReached = 0;
+	bool writingToOutputEnabled = false;
+	bool appendToFiles = false;
 
 	[[nodiscard]] static std::string getTime();
+	void checkBuffer();
 
-	inline void writeToOfstream(std::ostream& out) const { out.write(&buffer[0], buffer.size()); }
+	struct LineBreaker
+	{
+		Logger& parent;
+		bool passed = false;
+		LineBreaker(Logger& logger) : parent(logger) {}
+		LineBreaker(const LineBreaker&) = delete;
+		LineBreaker(LineBreaker&& rval) noexcept;
+		~LineBreaker();
+		LineBreaker operator<<(const std::string& s);
+	};
+	
+	friend LineBreaker;
+
+public:
+	explicit Logger(const std::string& name, bool appendToFiles = false, unsigned bufferSize = defaultBufferSize);
+	~Logger();
+
+	
+	[[nodiscard]] inline bool shouldWriteToFile() const					{ return buffer.size() >= flushWhenSizeWasReached; }
+
+	inline void clearBuffer()											{ buffer.clear(); }
+	inline void setBufferSize(unsigned newSize)							{ buffer.reserve(newSize); }
+	[[nodiscard]] inline auto getBufferSize() const						{ return buffer.capacity(); }
+
+	inline void setPathToLogs(std::string&& path)						{ pathToLogs = std::move(path); }
+	inline void setPathToLogs(const std::string& path)					{ pathToLogs = path; }
+	[[nodiscard]] inline const auto& getPathToLogs() const				{ return pathToLogs; }
+
+	inline static void setDefaultPathToLogs(std::string&& path)			{ defaultPathToLogs = std::move(path); }
+	inline static void setDefaultPathToLogs(const std::string& path)	{ defaultPathToLogs = path; }
+	[[nodiscard]] inline static const auto& getDefaultPathToLogs()		{ return defaultPathToLogs; }
+
+	inline void setWriteToOutput(bool flag)								{ writingToOutputEnabled = flag; }
+	inline void setOutput(std::ostream& out)							{ output = &out; }
+	[[nodiscard]] bool isWritingToOutputEnabled() const					{ return writingToOutputEnabled; }
+
+	void writeToOfstream();
+	void handleOutputWrite(const std::string & str) const;
+	LineBreaker operator<<(const std::string& s);
+
+	template <typename T>
+	[[nodiscard]] static std::string asString(T&& value);
 
 	template <typename... Args>
-		void message(Args&&... args);
-
-	template <typename T, typename... Args>
-		void _message(T&& obj, Args&&... args);
-
-	void _message() { buffer.push_back('\n'); }
+	void message(const std::string& formatter, Args&&... args);
 	
+	template <typename... Args>
+	void debug(const std::string& formatter, Args&&... args);
+
+	template <typename... Args>
+	void info(const std::string& formatter, Args&&... args);
+
+	template <typename... Args>
+	void trace(const std::string& formatter, Args&&... args);
+
+	template <typename... Args>
+	void warning(const std::string& formatter, Args&&... args);
+
+	template <typename... Args>
+	void error(const std::string& formatter, Args&&... args);
+
+	template <typename... Args>
+	void critical(const std::string& formatter, Args&&... args);
 };
 
+
 template <typename... Args>
-void Logger::message(Args&&... args)
+void Logger::message(const std::string& formatter, Args&&... args)
 {
-	auto string = std::move(getTime());
-	buffer.insert(buffer.end(), string.begin(), string.end());
-	_message(std::forward<Args>(args)...);
-}
-
-template <typename T, typename... Args>
-void Logger::_message(T&& obj, Args&&... args)
-{
-	auto string = asString(std::forward<T>(obj));
-	buffer.insert(buffer.end(), string.begin(), string.end());
-	_message(std::forward<Args>(args)...);
+	checkBuffer();
+	std::string extendedFormatter = "{} " + formatter + "\n";
+	std::string constructedMessage = std::format(extendedFormatter, getTime(), std::forward<Args>(args)...);
+	buffer.insert(buffer.end(), constructedMessage.begin(), constructedMessage.end());
+	handleOutputWrite(constructedMessage);
 }
 
 
+template <typename... Args>
+void Logger::debug(const std::string& formatter, Args&&... args){
+	message("{:11}" + formatter, DEBUG, std::forward<Args>(args)...);
+}
+
+
+template <typename... Args>
+void Logger::info(const std::string& formatter, Args&&... args){
+	message("{:11}" + formatter, INFO, std::forward<Args>(args)...);
+}
+
+
+template <typename... Args>
+void Logger::trace(const std::string& formatter, Args&&... args){
+	message("{:11}" + formatter, TRACE, std::forward<Args>(args)...);
+}
+
+
+template <typename... Args>
+void Logger::warning(const std::string& formatter, Args&&... args){
+	message("{:11}" + formatter, WARNING, std::forward<Args>(args)...);
+}
+
+
+template <typename... Args>
+void Logger::error(const std::string& formatter, Args&&... args){
+	message("{:11}" + formatter, ERROR, std::forward<Args>(args)...);
+}
+
+
+template <typename... Args>
+void Logger::critical(const std::string& formatter, Args&&... args){
+	message("{:11}" + formatter, CRITICAL, std::forward<Args>(args)...);
+}
+
+template <typename T>
+[[nodiscard]] static std::string Logger::asString(T&& value)
+{
+	if constexpr (std::is_same<std::decay<T>::type, const char*>::value || std::is_same<T, std::string>::value)
+		return value;
+	if constexpr (std::is_same<T, char>::value || std::is_same<T, wchar_t>::value)
+		return { value };
+	if constexpr (std::is_arithmetic<T>::value)
+		return std::to_string(value);
+	return "<UNSUPPORTED TYPE>";
+}
 
 }	//Log
 
+
+
+/*
+template <typename... Args>
+void Logger::message(Args&&... args)
+{
+	std::string formatter;
+	formatter.reserve((sizeof...(args)+2) * 3 + 2);
+	for (auto i = 0; i < sizeof...(args); i++)
+		formatter += "{} ";
+	formatter += "{}\n";
+	std::string constructedMessage = std::format(formatter, getTime(), std::forward<Args>(args)...);
+	buffer.insert(buffer.end(), constructedMessage.begin(), constructedMessage.end());
+}
+*/
